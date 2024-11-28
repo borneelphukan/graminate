c75caaf0934a131bfb6b0eb9a03c9fdb6216f09d
Borneel B. Phukan <borneelphukan@gmail.com>
2024-11-28 03:02:15 +0100

    u

--- src/routes/finder/+layout.svelte
+++ src/routes/finder/+layout.svelte
@@ -1,4 +1,19 @@
 <script lang="ts">
+	import SettingsBar from '../../components/layout/SettingsBar.svelte';
+	import NavPanel from '../../components/layout/NavPanel.svelte';
+
+	const buttons = [
+		{ name: 'Distributor', view: 'distributor' },
+		{ name: 'Supplier', view: 'supplier' },
+		{ name: 'Factories', view: 'factories' }
+	];
+
+	let activeView = 'distributor';
+
+	const handleNavigation = (event: CustomEvent<{ view: string }>) => {
+		activeView = event.detail.view;
+		console.log('Active view:', activeView);
+	};
 </script>
 
 <svelte:head>
@@ -11,8 +26,16 @@
 		<!-- <SettingsBar /> -->
 
 		<!-- Main Content -->
-		<div class="flex-1">
+		<div class="flex-1 relative">
+			<div class="absolute top-4 left-1/2 transform -translate-x-1/2 z-10">
+				<NavPanel {buttons} {activeView} on:navigate={handleNavigation} />
+			</div>
+
+			<!-- Map Content -->
 			<slot />
 		</div>
 	</div>
 </div>
+
+<style>
+</style>
