55d9d396fa45ab4c5130ec3616994cdd1d6b4618
Borneel B. Phukan <borneelphukan@gmail.com>
2025-01-01 16:53:16 +0100

    update

--- src/routes/platform/settings/general/+layout.svelte
+++ src/routes/platform/settings/general/+layout.svelte
@@ -2,46 +2,41 @@
 	import { page } from '$app/stores';
 	import { goto } from '$app/navigation';
 	import { derived } from 'svelte/store';
+	import { t } from '../../../../lib/i18n';
 	import NavPanel from '../../../../components/layout/NavPanel.svelte';
-	import Button from '../../../../components/ui/Button.svelte';
 
 	const navButtons = [
-		{ name: 'Profile', view: 'profile' },
-		{ name: 'Weather', view: 'weather' },
-		{ name: 'Milestones', view: 'milestones' },
-		{ name: 'Calendar', view: 'calendar' },
-		{ name: 'Tasks', view: 'tasks' },
-		{ name: 'Security', view: 'security' }
+		{ name: 'profile', view: 'profile' },
+		{ name: 'weather', view: 'weather' },
+		{ name: 'milestones', view: 'milestones' },
+		{ name: 'calendar', view: 'calendar' },
+		{ name: 'tasks', view: 'tasks' },
+		{ name: 'security', view: 'security' }
 	];
 
-	// Get the current view from the URL query
 	const currentView = derived(page, ($page) => $page.url.searchParams.get('view') || 'profile');
 
-	// Function to change the view
 	const changeView = (newView: string) => {
 		goto(`?view=${newView}`);
 	};
 </script>
 
 <svelte:head>
-	<title>Settings</title>
+	<title>{$t('settings')}</title>
 </svelte:head>
 
 <div class="flex min-h-screen">
 	<main class="flex-1 px-4">
-		<div class="pb-4 font-bold text-lg">General</div>
+		<div class="pb-4 font-bold text-lg">{$t('general')}</div>
 
-		<!-- NavPanel -->
 		<NavPanel
 			buttons={navButtons}
 			activeView={$currentView}
 			on:navigate={(e) => changeView(e.detail.view)}
 		/>
 
-		<!-- Page Content -->
 		<section>
 			<slot />
 		</section>
-
 	</main>
 </div>
