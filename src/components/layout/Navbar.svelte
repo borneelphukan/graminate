32fa67e58141f016e8ab37aaddd59ec5d8672031
Borneel B. Phukan <borneelphukan@gmail.com>
2025-01-14 16:23:13 +0100

    logo added

--- src/components/layout/Navbar.svelte
+++ src/components/layout/Navbar.svelte
@@ -3,6 +3,7 @@
 	import NotificationBar from './NotificationBar.svelte';
 	import ThemeSwitch from '@ui/ThemeSwitch.svelte';
 	import { t } from '@lib/i18n';
+	export let imageSrc: string = '/images/logo.png';
 
 	function toggleThemeHandler() {
 		console.log('Theme toggled');
@@ -46,14 +47,16 @@
 	};
 </script>
 
-<header class="bg-gray-800">
+<header class="bg-gray-800 py-2">
 	<div class="mx-auto max-w-7xl px-2 sm:px-4 lg:divide-y lg:divide-gray-700 lg:px-8">
 		<div class="relative flex h-12 py-1 justify-between">
 			<!-- Logo Section -->
 			<div class="relative z-10 flex px-2 lg:px-0">
 				<div class="flex flex-shrink-0 items-center">
-					<a href="/" class="text-left text-3xl font-semibold mb-2 text-white">
-						<span class="text-green-500">Farm</span>Mate
+					<a href="/" class="flex flex-row items-center gap-4">
+						<img src={imageSrc} alt="Graminate Logo" class="h-10 w-auto" />
+						<span class=" text-bold text-3xl text-light">Graminate</span>
+						<sup class=" text-bold text-lg text-light">ERP</sup>
 					</a>
 				</div>
 			</div>
