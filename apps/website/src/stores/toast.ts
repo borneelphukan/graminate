d9a59b88c25c2a5adbb98712ac44ce36857782d6
Borneel B. Phukan <borneelphukan@gmail.com>
2026-02-23 15:56:55 +0100

    remove unnecessary svelte codes

--- apps/website/src/stores/toast.ts
+++ apps/website/src/stores/toast.ts
@@ -1,4 +1,27 @@
-import { writable } from "svelte/store";
+type Subscriber<T> = (value: T) => void;
+
+function writable<T>(initialValue: T) {
+  let value = initialValue;
+  const subscribers = new Set<Subscriber<T>>();
+
+  return {
+    subscribe: (run: Subscriber<T>) => {
+      subscribers.add(run);
+      run(value);
+      return () => {
+        subscribers.delete(run);
+      };
+    },
+    set: (newValue: T) => {
+      value = newValue;
+      subscribers.forEach((subscriber) => subscriber(value));
+    },
+    update: (updater: (value: T) => T) => {
+      value = updater(value);
+      subscribers.forEach((subscriber) => subscriber(value));
+    }
+  };
+}
 
 export const toastMessage = writable<string | null>(null);
 export const showToast = writable<boolean>(false);
